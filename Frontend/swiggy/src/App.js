5aa5add7409135742afb4db630d8d5d876d397cf
Khushahal Sharma <113673132+Khushahal007@users.noreply.github.com>
2023-04-02 18:16:07 +0530

    Add files via upload

--- Frontend/swiggy/src/App.js
+++ Frontend/swiggy/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Components/Navbar'
 import HomeScreen from './Screens/HomeScreen'
@@ -7,6 +9,7 @@ import { BrowserRouter, NavLink, Route, Routes, Switch } from 'react-router-dom'
 import CardScreen from './Screens/CardScreen'
 import RegisterScreen from './Screens/RegisterScreen'
 import LoginScreen from './Screens/LoginScreen'
+import OrderScreen from './Screens/OrderScreen';
 
 const App = () => {
   return (
@@ -17,6 +20,7 @@ const App = () => {
         <Route path="/cart"  element={<CardScreen/>} />
         <Route path="/register"  element={<RegisterScreen/>} />
         <Route path="/login"  element={<LoginScreen/>} />
+        {/* <Route path="/orders"  element={<OrderScreen/>} /> */}
       </Routes>
 
     </div>
