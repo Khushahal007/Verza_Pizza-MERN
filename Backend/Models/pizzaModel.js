16a870a2376a84137ee9b2abb46b94204d7a70c8
Khushahal Sharma <113673132+Khushahal007@users.noreply.github.com>
2023-04-02 18:19:16 +0530

    Add files via upload

--- Backend/Models/pizzaModel.js
+++ Backend/Models/pizzaModel.js
@@ -2,27 +2,30 @@ const mongoose=require('mongoose');
 
 const pizzaSchema = mongoose.Schema({
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     variants: [String],
-    prices: [Number],
+    price: [{
+      small: Number,
+      medium: Number,
+      large: Number
+    }],
     category: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     image: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     }
-}, {
+  }, {
     timestamps: true
-});
-
+  });
 
 const pizzaModel= mongoose.model('pizzas', pizzaSchema)
 module.exports=pizzaModel
