3c952d289e087d20f9744bb70b62a56913945e95
Khushahal Sharma <113673132+Khushahal007@users.noreply.github.com>
2023-04-26 19:49:00 +0530

    Add files via upload

--- Frontend/swiggy/src/Reducers/pizzaReducers.js
+++ Frontend/swiggy/src/Reducers/pizzaReducers.js
@@ -1,19 +1,19 @@
-export const getAllPizzasReducer = (state={pizzas:[]}, action) =>{
+export const getAllPizzasReducer = (state = { pizzas: [] }, action) => {
 
-   switch(action.type){
-      case 'GET_PIZZAS_REQUEST': return {
-        loading: true,
-        ...state
-      }
-      case 'GET_PIZZAS_SUCCESS': return {
-        loading: false,
-        pizzas: action.payload
-      }
-      case 'GET_PIZZAS_ERROR': return {
-        loading: false,
-        pizzas: action.payload
-      }
+  switch (action.type) {
+    case 'GET_PIZZAS_REQUEST': return {
+      loading: true,
+      ...state
+    }
+    case 'GET_PIZZAS_SUCCESS': return {
+      loading: false,
+      pizzas: action.payload
+    }
+    case 'GET_PIZZAS_ERROR': return {
+      loading: false,
+      pizzas: []
+    }
 
-      default : return state
-   }
+    default: return state
+  }
 }
