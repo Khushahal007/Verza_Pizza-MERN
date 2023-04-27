d34f91895e24ee9628dc37988d1e93197730da46
Khushahal Sharma <113673132+Khushahal007@users.noreply.github.com>
2023-04-27 11:13:49 +0530

    Update HomeScreen.js

--- Frontend/swiggy/src/Screens/HomeScreen.js
+++ Frontend/swiggy/src/Screens/HomeScreen.js
@@ -1,38 +1,85 @@
+// import React from 'react'
+// import { getAllPizzas } from '../Actions/pizzaAction'
+// import Pizza from '../Components/Pizza'
+// import { useEffect, useState } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import Error from '../Components/Error'
+
+// const HomeScreen = () => {
+
+//   const dispatch = useDispatch();
+
+//   const pizzasstate = useSelector(state => state.getAllPizzasReducer)
+//   const { pizzas, error, loading } = pizzasstate
+
+//   useEffect(() => {
+//     dispatch(getAllPizzas())
+//   }, [])
+
+//   return (
+//     <div>
+//       <div className="row justify-content-center">
+//         {loading ? (
+//           <img style={{marginTop:'auto', width:'500px'}} src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw700" alt="loading GIF" />
+//         ) : error ? (
+//           <Error error='Something went Wrong'/>) : (
+//           pizzas.map((pizza) => {
+//             return (
+//               <div className=" col-md-3 m-3" key={pizza._id}>
+//                 <div >
+//                   <Pizza pizza={pizza} />
+
+//                 </div>
+//               </div>
+//             )
+//           })
+//         )}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default HomeScreen
+
+
 import React from 'react'
 import { getAllPizzas } from '../Actions/pizzaAction'
 import Pizza from '../Components/Pizza'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Error from '../Components/Error'
 
 const HomeScreen = () => {
-
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const pizzasstate = useSelector(state => state.getAllPizzasReducer)
   const { pizzas, error, loading } = pizzasstate
 
   useEffect(() => {
     dispatch(getAllPizzas())
-  }, [])
+  }, [dispatch])
 
   return (
     <div>
       <div className="row justify-content-center">
         {loading ? (
-          <img style={{marginTop:'auto', width:'500px'}} src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw700" alt="loading GIF" />
+          <img style={{ marginTop: 'auto', width: '500px' }} src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw700" alt="loading GIF" />
         ) : error ? (
-          <Error error='Something went Wrong'/>) : (
-          pizzas.map((pizza) => {
-            return (
-              <div className=" col-md-3 m-3" key={pizza._id}>
-                <div >
-                  <Pizza pizza={pizza} />
-
+          <Error error="Something went Wrong" />
+        ) : (
+          Array.isArray(pizzas) ? (
+            pizzas.map((pizza) => {
+              return (
+                <div className=" col-md-3 m-3" key={pizza._id}>
+                  <div >
+                    <Pizza pizza={pizza} />
+                  </div>
                 </div>
-              </div>
-            )
-          })
+              )
+            })
+          ) : (
+            <Error error="Pizzas data is not an array" />
+          )
         )}
       </div>
     </div>
