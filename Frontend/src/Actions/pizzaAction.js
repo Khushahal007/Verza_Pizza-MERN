4efd607abf7b389064f328cdc0396fbc0448a807
Khushahal Sharma <113673132+Khushahal007@users.noreply.github.com>
2023-09-26 17:14:40 +0530

    Update pizzaAction.js

--- Frontend/src/Actions/pizzaAction.js
+++ Frontend/src/Actions/pizzaAction.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-export const getAllPizzas = () =>async dispatch => {
+export const getAllPizzas = () => async dispatch => {
+  dispatch({ type: 'GET_PIZZAS_REQUEST' });
 
-    dispatch({ type: 'GET_PIZZAS_REQUEST' });
-
-    try {
-        const response = await axios.get('/api/pizzas/getallpizzas')
-        console.log(response)
-        dispatch({ type: 'GET_PIZZAS_SUCCESS', payload: response.data });
-    } catch (error) {
-        dispatch({ type: 'GET_PIZZAS_ERROR', payload: error });
-    }
-}
+  try {
+    const response = await axios.get('/api/pizzas/getallpizzas');
+    console.log(response);
+    dispatch({ type: 'GET_PIZZAS_SUCCESS', payload: response.data });
+  } catch (error) {
+    // Handle the error and provide a more specific error message in the payload
+    const errorMessage = error.response?.data?.message || 'An error occurred while fetching pizzas.';
+    dispatch({ type: 'GET_PIZZAS_ERROR', payload: errorMessage });
+  }
+};
