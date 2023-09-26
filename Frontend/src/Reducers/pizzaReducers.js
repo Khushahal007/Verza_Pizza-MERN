d44969a40c86dbce37b03ca1fa4d7cc7545a605f
Khushahal Sharma <113673132+Khushahal007@users.noreply.github.com>
2023-09-26 17:15:07 +0530

    Update pizzaReducers.js

--- Frontend/src/Reducers/pizzaReducers.js
+++ Frontend/src/Reducers/pizzaReducers.js
@@ -1,19 +1,24 @@
-export const getAllPizzasReducer = (state={pizzas:[]}, action) =>{
-
-   switch(action.type){
-      case 'GET_PIZZAS_REQUEST': return {
+export const getAllPizzasReducer = (state = { pizzas: [], loading: false, error: null }, action) => {
+  switch (action.type) {
+    case 'GET_PIZZAS_REQUEST':
+      return {
+        ...state,
         loading: true,
-        ...state
-      }
-      case 'GET_PIZZAS_SUCCESS': return {
+      };
+    case 'GET_PIZZAS_SUCCESS':
+      return {
+        ...state,
         loading: false,
-        pizzas: action.payload
-      }
-      case 'GET_PIZZAS_ERROR': return {
+        pizzas: action.payload,
+        error: null, // Reset the error to null on success
+      };
+    case 'GET_PIZZAS_ERROR':
+      return {
+        ...state,
         loading: false,
-        pizzas: action.payload
-      }
-
-      default : return state
-   }
-}
+        error: action.payload, // Set the error message
+      };
+    default:
+      return state;
+  }
+};
